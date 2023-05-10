3295b5ebb1931c0d1072ac42a91c88571c4160a0
DeGGi <81147016+DeGGiii@users.noreply.github.com>
2023-05-10 15:49:08 +0200

    Rensat kod och kommenterat för enhetstester

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,52 +1,54 @@
-const { Builder, By, until } = require('selenium-webdriver');
-require('geckodriver');
+const { Builder, By, until } = require("selenium-webdriver");
+require("geckodriver");
 
-const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
+const fileUnderTest =
+  "file://" + __dirname.replace(/ /g, "%20") + "/../dist/index.html";
 const defaultTimeout = 10000;
 let driver;
 jest.setTimeout(1000 * 60 * 5); // 5 minuter
 
 // Det här körs innan vi kör testerna för att säkerställa att Chrome är igång
 beforeAll(async () => {
-console.log(fileUnderTest);
-    driver = await new Builder().forBrowser('chrome').build();
-    await driver.get(fileUnderTest);
+  console.log(fileUnderTest);
+  driver = await new Builder().forBrowser("chrome").build();
+  await driver.get(fileUnderTest);
 });
 
 // Allra sist avslutar vi chrome igen
-afterAll(async() => {
-    await driver.quit();
+afterAll(async () => {
+  await driver.quit();
 }, defaultTimeout);
 
-test('The stack should be empty in the beginning', async () => {
-	let stack = await driver.findElement(By.id('top_of_stack')).getText();
-	expect(stack).toEqual("n/a");
+test("The stack should be empty in the beginning", async () => {
+  let stack = await driver.findElement(By.id("top_of_stack")).getText();
+  expect(stack).toEqual("n/a");
 });
 
-//New test
-test('Add an item to the stack', async() => {
-  
-	// Klicka "Pusha til stacken" knappen för att aktivera alerten.
-	await driver.findElement(By.id('push')).click();
-  
-	// Byta till alert page och skriver in Coconut i boxen som value
-	const alert = await driver.switchTo().alert();
-	await alert.sendKeys('Coconut');
-	await alert.accept();
-  
-	// Hämta den nya uppdaterade texten som gick in i stacken
-	const updatedStackItem = await driver.findElement(By.id('top_of_stack')).getText();
-  
-	// Kollar så att texten som lades in i stacken stämmer med toEqual().
-	expect(updatedStackItem).toEqual('Coconut');
+// ** Nytt test som kollar att ett item lades till i stacken **
+test("Add an item to the stack", async () => {
+  // Klicka "Pusha til stacken" knappen för att aktivera alerten.
+  await driver.findElement(By.id("push")).click();
+
+  // Byta till alert page och skriver in Coconut i boxen som value
+  const alert = await driver.switchTo().alert();
+  await alert.sendKeys("Coconut");
+  await alert.accept();
+
+  // Hämta den nya uppdaterade texten som gick in i stacken
+  const updatedStackItem = await driver
+    .findElement(By.id("top_of_stack"))
+    .getText();
+
+  // Kollar så att texten som lades in i stacken stämmer med toEqual().
+  expect(updatedStackItem).toEqual("Coconut");
 });
 
 describe('Clicking "Pusha till stacken"', () => {
-	it('should open a prompt box', async () => {
-		let push = await driver.findElement(By.id('push'));
-		await push.click();
-		let alert = await driver.switchTo().alert();
-		await alert.sendKeys("Bananer");
-		await alert.accept();
-	});
+  it("should open a prompt box", async () => {
+    let push = await driver.findElement(By.id("push"));
+    await push.click();
+    let alert = await driver.switchTo().alert();
+    await alert.sendKeys("Bananer");
+    await alert.accept();
+  });
 });
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,33 +1,32 @@
-const stack = require('../src/stack');
+const stack = require("../src/stack");
 
-test('peek on empty stack returns undefined', () => {
-    expect(stack.peek()).toBeUndefined();
+test("peek on empty stack returns undefined", () => {
+  expect(stack.peek()).toBeUndefined();
 });
 
-test('peek on stack with one element returns that element', () => {
-    stack.push(1);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(1);
+test("peek on stack with one element returns that element", () => {
+  stack.push(1);
+  expect(stack.peek()).toBeDefined();
+  expect(stack.peek()).toBe(1);
 });
 
-test('peek on stack with two or more elements returns the top element', () => {
-    stack.push(1);
-    stack.push("wow");
-    stack.push(42);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(42);
+test("peek on stack with two or more elements returns the top element", () => {
+  stack.push(1);
+  stack.push("wow");
+  stack.push(42);
+  expect(stack.peek()).toBeDefined();
+  expect(stack.peek()).toBe(42);
 });
 
-//My new own test
-test('Lägg till ett item i stacken', () => {
-    stack.push('item1');
-    expect(stack.peek()).toBe('item1');
-  });
-  
-  test('Ta bort valuen som ligger högst upp i stacken', () => {
-    stack.push('item1');
-    stack.push('item2');
-    expect(stack.pop()).toBe('item2');
-    expect(stack.pop()).toBe('item1');
-  });
-  
+// ** Två nya enhetstester **
+test("Lägg till ett item i stacken", () => {
+  stack.push("item1");
+  expect(stack.peek()).toBe("item1");
+});
+
+test("Ta bort valuen som ligger högst upp i stacken", () => {
+  stack.push("item1");
+  stack.push("item2");
+  expect(stack.pop()).toBe("item2");
+  expect(stack.pop()).toBe("item1");
+});
